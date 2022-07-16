9bb9576665ba7eedc8af8c212994827f2ccad336
asukayang7 <105493735+asukayang7@users.noreply.github.com>
2022-07-17 02:18:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -34,14 +34,14 @@ global.price1 = '*List sewa*\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMA
 
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = './media/zyrexx.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
-global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379e.jpg' //ganti pakai poto payment atau apa
+global.fotonya2 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini juga ganti pakai foto bot mu
+global.fotonya3 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ini juga ganti
+global.fotonya8 = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //foto session
+global.donasi = 'https://telegra.ph/file/7319a2259b4a656f98213.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'Fadli'
